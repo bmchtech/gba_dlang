95d3a4aa33fd512eca2ccaf0fa196601e0f0416e
redthing1 <xhyinne@gmail.com>
2021-11-16 22:07:03 -0800

    draw more stuff in the demo rom

--- demo/src/main.d
+++ demo/src/main.d
@@ -7,13 +7,27 @@ extern(C) int main() {
 	printf("Hello, world!\n");
     
     *REG_DISPCNT = DCNT_MODE0 | DCNT_BG0;
-    pal_bg_mem[0] = 0x0C02; // background color
+    pal_bg_mem[0] = 0x6B9D; // background color
 
-	if (mgba_open()) {
-		mgba_printf(MGBA_LOG_LEVEL.MGBA_LOG_ERROR, "Hello, world, %s!\n", cast(const char*)"Bean Machine");
-	} else {
-        // failed to open mgba
-        pal_bg_mem[0] = CLR_RED;
+    // write some text to screen
+    *REG_DISPCNT |= DCNT_BG1;
+	tte_init_chr4c(1, cast(u16) (BG_CBB!u16(0)|BG_SBB!u16(31)), 0, 0x0201, CLR_WHITE, null, null);
+	tte_init_con();
+
+    // set text colors
+    pal_bg_mem[1] = 0x39BB;
+    pal_bg_mem[2] = 0x5AAE;
+    pal_bg_mem[3] = 0x2D09;
+    pal_bg_mem[4] = 0x4F54;
+
+    tte_printf("#{P:12,12}#{ci:1}bean #{ci:2}machine");
+    tte_printf("#{P:20,26}#{ci:3}dlang on gba");
+    tte_printf("#{P:76,26}#{ci:4}success");
+
+    bool mgba_opened = mgba_open();
+    if (mgba_opened) {
+        mgba_printf(MGBA_LOG_LEVEL.MGBA_LOG_ERROR, "Hello, from %s!\n", cast(const char*)"Bean Machine");
+        tte_printf("#{P:20,40}#{ci:4}mgba opened");
     }
 
     while (TRUE) {
